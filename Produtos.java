0aabda11d3190df1590328990d65dc15403199d2
gleibsonf <31158825+gleibsonf@users.noreply.github.com>
2017-08-28 10:49:42 -0300

    Update Produtos.java
    Adicionei as condições para as categorias dos produtos e coloquei o valor total dos produtos!

--- Produtos.java
+++ Produtos.java
@@ -1,46 +1,101 @@
+package PROJETO;
 
-public class Produtos {
-	
-	private String nome;
-	private String categoria;
-	private double preco;
-	private int codigo;
-	
-	public String getNome() {
-		return nome;
+public class Produto {
+	private int codProduto;
+    private String nomeProduto;
+    private String marcaProduto;
+    private double valorProduto;
+    private int quantidadeProduto;
+    private String categoriaProduto;
+	private double valorTotal;
+
+
+    public int getCodProduto() {
+       
+        return codProduto;
+
+    }
+    public void setCodProduto(int codProduto) {
+        
+        this.codProduto = codProduto;
+
+    }
+
+
+    public String getNomeProduto() {
+
+        return nomeProduto;
+
+    }
+    public void setNomeProduto(String nomeProduto) {
+
+        this.nomeProduto = nomeProduto;
+
+    }
+
+
+    public String getMarcaProduto() {
+
+        return marcaProduto;
+
+    }
+    public void setMarcaProduto(String marcaProduto) {
+        
+        this.marcaProduto = marcaProduto;
+
+    }
+
+
+    public double getValorProduto() {
+        
+        return valorProduto;
+
+    }
+    
+    public void setValorProduto(double valorProduto) {
+        
+        if (valorProduto <= 0)
+
+        	System.out.println("Erro valor do produto não pode ser menor que zero!");
+        
+        else
+
+        this.valorProduto = valorProduto;
+    }
+
+
+    public int getQuantidadeProduto() {
+        return quantidadeProduto;
+    }
+    public void setQuantidadeProduto(int quantidadeProduto) {
+        if (quantidadeProduto > 0)
+        this.quantidadeProduto = quantidadeProduto;
+        else
+            System.out.println("Erro o produto não pode ser menor ou igual zero");
+    }
+
+
+    public String getCategoriaProduto() {
+        return categoriaProduto;
+    }
+    public void setCategoriaProduto(String categoriaProduto) {
+    	if (this.valorProduto > 25.50)
+    		this.categoriaProduto = "A";
+    	if (this.valorProduto < 25.50 && this.valorProduto > 15.50)
+    		this.categoriaProduto = "B";
+    	if (this.valorProduto < 15.50 && this.valorProduto > 7.50)
+    		this.categoriaProduto = "C";
+    	else
+    		this.categoriaProduto = "D";
+        this.categoriaProduto = categoriaProduto;
+    }
+    
+    public double getValorTotal() {
+    	this.valorTotal = quantidadeProduto * valorProduto;
+		return valorTotal;
 	}
-	
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
-	public String getCategoria(){
-		return categoria;
-	}
-	
-	public void setCategoria(String categoria) {
-		this.categoria = categoria;
-	}
-	
-	public double getPreco() {
-		return preco;
-	}
-	
-	public void setPreco(double preco) {
-		if (preco > 0) {
-			this.preco = preco;
-			System.out.println("Produto cadastrado com sucesso!");
-		}else {
-			System.out.println("Produto não cadastrado");
-		}
-		
-	}
-	
-	public int getCodigo() {
-		return codigo;
-	}
-	
-	public void setCodigo(int codigo) {
-		this.codigo = codigo;
+    public void setValorTotal(double valorTotal) {
+		this.valorTotal = valorTotal;
 	}
 }
+
