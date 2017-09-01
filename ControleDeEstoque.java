f46ce9844b92178875d1461c22d27afe2858ec15
gleibsonf <31158825+gleibsonf@users.noreply.github.com>
2017-09-01 10:44:28 -0300

    Update ControleDeEstoque.java

--- ControleDeEstoque.java
+++ ControleDeEstoque.java
@@ -1,4 +1,4 @@
-package PROJETO;
+package Projeto;
 
 
 
@@ -8,56 +8,174 @@ public class ControleDeEstoque {
     private String dataEntrada;
     private String dataSaida;
     private double dinheiroEstoque;
+    private double novoSaldo;
     private static int produtoEstoque;
+    private int comprar;
+    private int vender;
+    private int escolhaProduto;
+    
 
 
-    ControleDeEstoque(){
+    public ControleDeEstoque(Fornecedor fornecedor, Produto produto){
         this.fornecedor = fornecedor;
         this.produto = produto;
         this.dataEntrada = dataEntrada;
-        this.dataSaida = dataSaida;
-        this.dinheiroEstoque= 100000;
+        this.dinheiroEstoque = dinheiroEstoque;
     }
 
-    public void addProduto(Produto produto){
-    	this.produto = produto;
-    	this.produtoEstoque++; 
-
-    }
     
+
+	public void Produto(double valor,Produto produto, String dataEntrada){
+        this.produto = produto;
+        this.dataEntrada = dataEntrada;
+        this.produtoEstoque++;
+        this.dinheiroEstoque -= valor;
+
+
+    }
+
     public String getDataEntrada() {
-		return dataEntrada;
-	}
-    
+        return dataEntrada;
+    }
+
     public void setDataEntrada(String dataEntrada) {
-    	if (dataEntrada.length() == 10)
-            this.dataEntrada = dataEntrada;
-            else
-                System.out.println("Erro a data de entrada deve seguir este formato(XX/XX/XXXX)");
-	}
+          if (dataEntrada.length() == 10)
+        	  this.dataEntrada = dataEntrada;
+
+         
+        	  System.out.println("Erro a data de entrada deve seguir este formato(XX/XX/XXXX)");
+          }
+                
     
+
     public String getDataSaida() {
-		return dataSaida;
-	}
-    
+        return dataSaida;
+    }
+
     public void setDataSaida(String dataSaida) {
     	if (dataSaida.length() == 10)
-    		this.dataSaida = dataSaida;
-            else
-                System.out.println("Erro a data de saída deve seguir este formato(XX/XX/XXXX)");
-	}
-    
+            this.dataSaida = dataSaida;
+ 
+            
+            System.out.println("Erro a data de saída deve seguir este formato(XX/XX/XXXX)");
+    }
+
     public double getDinheiroEstoque() {
-		return dinheiroEstoque;
-	}
-    
+        return dinheiroEstoque;
+    }
+
     public void setDinheiroEstoque(double dinheiroEstoque) {
-    	if (this.dinheiroEstoque < Produto.getValorTotal())
-    		System.out.println("Você não tem dinhero suficiente em estoque!");
-    	else
-		this.dinheiroEstoque = dinheiroEstoque;
+        this.dinheiroEstoque = dinheiroEstoque;
+    }
+    public double getNovoSaldo() {
+		return novoSaldo;
+	}
+    public int getComprar() {
+    	this.comprar = comprar - produto.getQuantidadeProduto();
+		return comprar;
+	}
+    public void setComprar(int comprar) {
+    	if (this.comprar > 0)
+		this.comprar = comprar;
+		this.novoSaldo = this.dinheiroEstoque -(produto.getValorProduto() * this.comprar);
+		
+	}
+    public int getVender() {
+    	this.vender = vender + produto.getQuantidadeProduto();
+		return vender;
+	}
+    public void setVender(int vender) {
+    	
+		this.vender = vender;
+		this.novoSaldo = this.dinheiroEstoque +(produto.getValorProduto() * this.vender);
+	}
+    public int getEscolhaProduto() {
+    	this.escolhaProduto = produto.getCodProduto();
+		return escolhaProduto;
+	}
+    public void setEscolhaProduto(int escolhaProduto) {
+		this.escolhaProduto = escolhaProduto;
 	}
 
 
+    
+    
+    
+    
+    
+    
+    // Depois...
+}
+class Data{
+	private Fornecedor fornecedor;
+    private Produto produto;
+	private int dia;
+	private int mes;
+	private int ano;
+	private String novaData;
+
+	
+	public int getDia() {
+		return dia;
+	}
+	public int length() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	public void setDia(int dia) {
+		this.dia = dia;
+	}
+	
+	public int getMes() {
+		return mes;
+	}
+	public void setMes(int mes) {
+		this.mes = mes;
+	}
+	
+	public int getAno() {
+		return ano;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+	public String formatarData(){
+		this.novaData = (this.dia + "/" + this.mes + "/" + this.ano);
+		return novaData;
+	}
+	
+	public String getData(){
+		String dataFormatada = formatarData();
+		return  dataFormatada;
+	}
+	
+
+	public void setData(Data data, int dia, int mes, int ano){
+		do{
+			if (dia <= 31 && dia > 0) {
+			this.dia = data.dia;	
+		}else{
+			System.out.println("Formato inválido! digite apenas os 2 digitos do dia!");
+		}
+		}while (dia == data.dia);
+		do{
+		if (data.mes <= 12 && data.mes > 0) {
+			this.mes = data.mes;	
+		}else{
+			System.out.println("Formato inválido! digite apenas os 2 digitos do mês!");		
+		}
+		}while (mes == data.mes);
+		do{
+			if (data.ano > 0) {
+			this.ano = data.ano;	
+		}else{
+				System.out.println("Formato inválido! digite apenas os 4 digitos do ano!");		
+			}
+		}while (ano == data.ano);
+		
+		
+	
+}
 
 }
