9b2ab1ae5075b57af6e684b7fed1ad34529bf7c8
gleibsonf <31158825+gleibsonf@users.noreply.github.com>
2017-09-01 21:33:02 -0300

    Update Programa.java

--- Programa.java
+++ Programa.java
@@ -6,29 +6,68 @@ public class Programa {
 
 	public static void main(String[] args) {
 		
-		Produto p = new Produto();
-		p.setCodProduto(1345);
-		p.setNomeProduto("Carro");
-		p.setMarcaProduto("Fiat");
-		p.setQuantidadeProduto(10);
-		p.setValorProduto(50.0);
+		Produto p[] = new Produto[10];
 		
-	
-	
+		p[0].setCodProduto(1234);
+		p[0].setNomeProduto("Carro Palio");
+		p[0].setMarcaProduto("Fiat");
+		p[0].setQuantidadeProduto(100);
+		p[0].setValorProduto(50000.00);
 		
-		Fornecedor f = new Fornecedor();
-		f.setNome("Linx");
-		f.setCnpj("22.555.000/0001-11");
-		f.setEmail("Jonas@gmail.com");
-		f.setLote("12/07/2017");
-		f.setTelefone("081 98835-9262");
+		p[1].setCodProduto(2234);
+		p[1].setNomeProduto("Carro new Fiesta");
+		p[1].setMarcaProduto("Ford");
+		p[1].setQuantidadeProduto(200);
+		p[1].setValorProduto(53000.00);
 		
-
+		p[2].setCodProduto(3234);
+		p[2].setNomeProduto("Carro HB 20");
+		p[2].setMarcaProduto("Hyundai");
+		p[2].setQuantidadeProduto(200);
+		p[2].setValorProduto(44000.00);
 		
+		p[3].setCodProduto(4234);
+		p[3].setNomeProduto("Carro Classic");
+		p[3].setMarcaProduto("Chevrolet");
+		p[3].setQuantidadeProduto(150);
+		p[3].setValorProduto(22000.00);
 		
+		p[4].setCodProduto(4234);
+		p[4].setNomeProduto("Carro Fox");
+		p[4].setMarcaProduto("Volkswagen");
+		p[4].setQuantidadeProduto(70);
+		p[4].setValorProduto(42000.00);
+		
+		Fornecedor f[] = new Fornecedor[10];
+		
+		f[0].setNome("Fiat");
+		f[0].setCnpj("00.000.0000/0001-00");
+		f[0].setTelefone("81 997133813");
+		f[0].setEmail("fiat@gmail.com");
+		
+		f[1].setNome("Ford");
+		f[1].setCnpj("00.000.0000/0002-00");
+		f[1].setTelefone("81 98454-5415");
+		f[1].setEmail("ford@gmail.com");
+		
+		f[2].setNome("Hyunday");
+		f[2].setCnpj("00.000.0000/0003-00");
+		f[2].setTelefone("81 99453-3813");
+		f[2].setEmail("hyunday@gmail.com");
+		
+		f[3].setNome("Chevrolet");
+		f[3].setCnpj("00.000.0000/0004-00");
+		f[3].setTelefone("81 99713-3813");
+		f[3].setEmail("chevrolet@gmail.com");
+		
+		f[4].setNome("Volkswagen");
+		f[4].setCnpj("00.000.0000/0005-00");
+		f[4].setTelefone("81 99713-4853");
+		f[4].setEmail("volkswagen@gmail.com");
 		
 		ControleDeEstoque ce = new ControleDeEstoque(f, p);
-		ce.setDinheiroEstoque(200.0);
+		
+		ce.setDinheiroEstoque(20000.0);
 		ce.setDataSaida("15/05/2017");
 		ce.setVender(2);
 		ce.setEscolhaProduto(1343);
